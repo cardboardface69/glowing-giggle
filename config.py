cb739a392465e248423ef2656ecb4cf3968f3d34
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-01-16 00:21:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "10247139"
+
+API_HASH = "96b46175824223a33737657ab943fd6a"
+
+BOT_TOKEN = "5607218709:AAFBTEY8N479gkZZtJabErHkIsuXnOe87Iw"
+
+MONGO_DB_URI = "mongodb+srv://web:test@cluster0.zz54kvj.mongodb.net/?retryWrites=true&w=majority"
+
+INDEX_ID = -1001769643047
+
+UPLOADS_ID = -1001769643047
+
+PROGRESS_ID = -1001769643047
+
+LINK_ID = -1001769643047
+
+STATUS_ID = 2
+
+SCHEDULE_ID = 3
+
+CHANNEL_TITLE = "Animxt"
+
+INDEX_USERNAME = "Latest_ongoing_airing_anime"
+
+UPLOADS_USERNAME = "Latest_ongoing_airing_anime"
